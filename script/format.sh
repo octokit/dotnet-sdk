524583ed760ec19f4ca9a5368a4f8a45dac86842
Keegan Campbell <me@kfcampbell.com>
2024-01-30 14:29:25 -0800

    Rip out unmaintained analysis Action

--- script/format.sh
+++ script/format.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# GitHub Actions performs linting in .github/workflows/build.yml.
+# GitHub Actions performs formatting in .github/workflows/build.yml.
 # This script may be used to update files before pushing to a PR check.
 dotnet format
