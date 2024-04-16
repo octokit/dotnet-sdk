d0f78e999db29553da446f21d6a5f65643227d98
Keegan Campbell <me@kfcampbell.com>
2024-04-16 16:06:04 -0700

    Simplify example CLI

--- cli/Program.cs
+++ cli/Program.cs
@@ -1,47 +1,22 @@
 ﻿using GitHub;
 using GitHub.Octokit.Client;
 using GitHub.Octokit.Authentication;
-using System.Threading;
-using GitHub.Zen;
 
 var token = Environment.GetEnvironmentVariable("GITHUB_TOKEN") ?? "";
-var request = RequestAdapter.Create(new TokenAuthenticationProvider("Octokit.Gen", token));
-request.BaseUrl = "http://api.github.localhost:1024";
-var gitHubClient = new GitHubClient(request);
+var adapter = RequestAdapter.Create(new TokenAuthenticationProvider("Octokit.Gen", token));
+adapter.BaseUrl = "http://api.github.localhost:1024";
+var gitHubClient = new GitHubClient(adapter);
 
-var requests = new List<Tuple<int, Task<string?>>>();
+var requests = new List<Task<string?>>();
 
 for (var i = 0; i < 1000; i++)
 {
-    Tuple<int, Task<string?>> numberedRequest = new Tuple<int, Task<string?>>(i, gitHubClient.Zen.GetAsync());
-    requests.Add(numberedRequest);
-    // try
-    // {
-    //     var zen = await gitHubClient.Zen.GetAsync();
-    //     Console.WriteLine($"Result {i}: {zen}");
-    // }
-    // catch (Exception e)
-    // {
-    //     Console.WriteLine(e.Message);
-    //     Environment.Exit(1);
-    // }
+    var request = gitHubClient.Zen.GetAsync();
+    requests.Add(request);
 }
 
-var responses = await Task.WhenAll(requests.Select(async request =>
-{
-    var (i, task) = request;
-    try
-    {
-        var zen = await task;
-        return $"Result {i}: {zen}";
-    }
-    catch (Exception e)
-    {
-        return $"Result {i}: {e.Message}";
-    }
-}));
-
+var responses = await Task.WhenAll(requests);
 foreach (var response in responses)
 {
-    Console.WriteLine(response);
+    Console.WriteLine($"Response: {response}");
 }
