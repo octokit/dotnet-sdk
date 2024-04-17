e42a0bcc17e17e739aee5ac95db4e5d2090c446e
Keegan Campbell <me@kfcampbell.com>
2024-04-17 16:24:26 -0700

    CLI tweaks for triggering secondary rate limiting

--- cli/Program.cs
+++ cli/Program.cs
@@ -9,22 +9,23 @@ var gitHubClient = new GitHubClient(adapter);
 
 // serial requests may trigger primary rate limiting but are unlikely
 // to trigger secondary rate limiting
-for (var i = 0; i < 1000; i++)
+// for (var i = 0; i < 1000; i++)
+// {
+//     var response = await gitHubClient.Zen.GetAsync();
+//     Console.WriteLine($"Response {i}: {response}");
+// }
+
+var requests = new List<Task<string?>>();
+for (var i = 0; i < 1500; i++)
 {
-    var response = await gitHubClient.Zen.GetAsync();
-    Console.WriteLine($"Response {i}: {response}");
+    var request = gitHubClient.Zen.GetAsync();
+    requests.Add(request);
 }
 
-// var requests = new List<Task<string?>>();
-
-// for (var i = 0; i < 150; i++)
-// {
-//     var request = gitHubClient.Zen.GetAsync();
-//     requests.Add(request);
-// }
-
-// var responses = await Task.WhenAll(requests);
-// foreach (var response in responses)
-// {
-//     Console.WriteLine($"Response: {response}");
-// }
+var responses = await Task.WhenAll(requests);
+var responseNumber = 0;
+foreach (var response in responses)
+{
+    Console.WriteLine($"Response {responseNumber}: {response}");
+    responseNumber++;
+}
