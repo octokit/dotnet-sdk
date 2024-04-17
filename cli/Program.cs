4a058b29983eebfab98b6eda315a21530ca8990f
Keegan Campbell <me@kfcampbell.com>
2024-04-17 15:22:44 -0700

    Program.cs switches to serial requests, is formatted

--- cli/Program.cs
+++ cli/Program.cs
@@ -1,22 +1,30 @@
 ﻿using GitHub;
-using GitHub.Octokit.Client;
 using GitHub.Octokit.Authentication;
+using GitHub.Octokit.Client;
 
 var token = Environment.GetEnvironmentVariable("GITHUB_TOKEN") ?? "";
 var adapter = RequestAdapter.Create(new TokenAuthenticationProvider("Octokit.Gen", token));
 adapter.BaseUrl = "http://api.github.localhost:1024";
 var gitHubClient = new GitHubClient(adapter);
 
-var requests = new List<Task<string?>>();
-
+// serial requests may trigger primary rate limiting but are unlikely
+// to trigger secondary rate limiting
 for (var i = 0; i < 1000; i++)
 {
-    var request = gitHubClient.Zen.GetAsync();
-    requests.Add(request);
-}
-
-var responses = await Task.WhenAll(requests);
-foreach (var response in responses)
-{
+    var response = await gitHubClient.Zen.GetAsync();
     Console.WriteLine($"Response: {response}");
 }
+
+// var requests = new List<Task<string?>>();
+
+// for (var i = 0; i < 150; i++)
+// {
+//     var request = gitHubClient.Zen.GetAsync();
+//     requests.Add(request);
+// }
+
+// var responses = await Task.WhenAll(requests);
+// foreach (var response in responses)
+// {
+//     Console.WriteLine($"Response: {response}");
+// }
