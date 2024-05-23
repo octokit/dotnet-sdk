ee883d53d326dcf7482c5e1e76f2bae0674edbb5
Nick Floyd <139819+nickfloyd@users.noreply.github.com>
2024-05-23 14:22:48 -0500

    Implements refresh tokens

--- src/Middleware/Authentication/AppInstallationTokenProvider.cs
+++ src/Middleware/Authentication/AppInstallationTokenProvider.cs
@@ -11,10 +11,16 @@ namespace GitHub.Octokit.Client.Authentication;
 
 public class AppInstallationTokenProvider : IAccessTokenProvider
 {
+
+    private int _timeoutMinuites = 10;
+
     private readonly string _sourceId;
     private readonly RSA _privateKeyPath;
     private readonly string _installationId;
     private string _accessToken = string.Empty;
+
+    private SecurityTokenDescriptor? _tokenDescriptor;
+
     AllowedHostsValidator IAccessTokenProvider.AllowedHostsValidator => new AllowedHostsValidator();
 
     public AppInstallationTokenProvider(int appId, RSA privateKeyPath, string installationId)
@@ -33,7 +39,7 @@ public class AppInstallationTokenProvider : IAccessTokenProvider
 
     public async Task<string> GetAuthorizationTokenAsync(Uri requestUri, Dictionary<string, object>? additionalAuthenticationContext = default, CancellationToken cancellationToken = default)
     {
-        if (string.IsNullOrEmpty(_accessToken))
+        if (string.IsNullOrEmpty(_accessToken) || (_tokenDescriptor != null && _tokenDescriptor.Expires < DateTime.UtcNow.AddMinutes(-1)))
         {
             var jwt = CreateJsonWebToken(_privateKeyPath);
             var baseUrl = requestUri.GetLeftPart(UriPartial.Authority);
@@ -45,17 +51,13 @@ public class AppInstallationTokenProvider : IAccessTokenProvider
     }
 
     // TODO: abstract this out 
-    public Task<string> RefreshAppInstallationAccessTokenAsync(int installationId) => throw new NotImplementedException();
-
-    // TODO: abstract this out 
-    // Come up with a way to pass base URL
     private async Task<string> GetGitHubAccessTokenAsync(string baseUrl, string jwt, string installationId)
     {
         using var client = new HttpClient();
 
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt);
-        var request = new HttpRequestMessage(HttpMethod.Post, $"{baseUrl}/app/installations/{installationId}/access_tokens");
 
+        var request = new HttpRequestMessage(HttpMethod.Post, $"{baseUrl}/app/installations/{installationId}/access_tokens");
         request.Headers.Add("Accept", "application/vnd.github.v3+json");
         request.Headers.Add("User-Agent", "YourAppName");
 
@@ -75,22 +77,30 @@ public class AppInstallationTokenProvider : IAccessTokenProvider
         var now = DateTime.UtcNow;
         var tokenHandler = new JsonWebTokenHandler();
         var signingCredentials = new SigningCredentials(new RsaSecurityKey(privateKey), SecurityAlgorithms.RsaSha256);
+
+        CreateTokenDescriptor(signingCredentials, now);
+
+        return tokenHandler.CreateToken(_tokenDescriptor);
+    }
+
+    private void CreateTokenDescriptor(SigningCredentials credentials, DateTime now)
+    {
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Issuer = _sourceId,
             IssuedAt = now,
             NotBefore = now,
-            Expires = now.AddMinutes(10),
-            SigningCredentials = signingCredentials,
+            Expires = now.AddMilliseconds(_timeoutMinuites),
+            SigningCredentials = credentials,
             Claims = new Dictionary<string, object>
           {
               { "iat", new DateTimeOffset(now).ToUnixTimeSeconds() },
-              { "exp", new DateTimeOffset(now.AddMinutes(10)).ToUnixTimeSeconds() },
+              { "exp", new DateTimeOffset(now.AddMilliseconds(_timeoutMinuites)).ToUnixTimeSeconds() },
               { "iss", _sourceId }
           }
         };
-
-        return tokenHandler.CreateToken(tokenDescriptor);
+        _tokenDescriptor = tokenDescriptor;
     }
+
 
 }
