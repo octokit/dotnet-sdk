3d1d49ada3bd0f359e6ad9383ddb06019ce801b6
Nick Floyd <139819+nickfloyd@users.noreply.github.com>
2024-05-23 11:14:11 -0500

    dotnet format

--- test/Middleware/Authentication/AppInstallationTokenProviderTest.cs
+++ test/Middleware/Authentication/AppInstallationTokenProviderTest.cs
@@ -1,27 +1,27 @@
-using Xunit;
-using Moq;
 using System.Security.Cryptography;
 using GitHub.Octokit.Client.Authentication;
+using Moq;
+using Xunit;
 
 public class AppInstallationTokenProviderTests
 {
-  private readonly Mock<RSA> _mockPrivateKey;
-  private readonly AppInstallationTokenProvider _provider;
+    private readonly Mock<RSA> _mockPrivateKey;
+    private readonly AppInstallationTokenProvider _provider;
 
-  public AppInstallationTokenProviderTests()
-  {
-    _mockPrivateKey = new Mock<RSA>();
-    _provider = new AppInstallationTokenProvider("clientId", _mockPrivateKey.Object, "installationId");
-  }
+    public AppInstallationTokenProviderTests()
+    {
+        _mockPrivateKey = new Mock<RSA>();
+        _provider = new AppInstallationTokenProvider("clientId", _mockPrivateKey.Object, "installationId");
+    }
 
-  // TODO: Implement tests
-  // [Fact]
-  // public async Task GetAuthorizationTokenAsync_WhenCalled_ReturnsToken()
-  // {
-  //   var requestUri = new Uri("https://api.github.com");
-  //   var result = await _provider.GetAuthorizationTokenAsync(requestUri);
+    // TODO: Implement tests
+    // [Fact]
+    // public async Task GetAuthorizationTokenAsync_WhenCalled_ReturnsToken()
+    // {
+    //   var requestUri = new Uri("https://api.github.com");
+    //   var result = await _provider.GetAuthorizationTokenAsync(requestUri);
 
-  //   Assert.NotNull(result);
-  //   Assert.IsType<string>(result);
-  // }
+    //   Assert.NotNull(result);
+    //   Assert.IsType<string>(result);
+    // }
 }
