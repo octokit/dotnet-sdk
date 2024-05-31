d8578bb431f05cc96a1d70fc3d2de16ead56fcc2
Keegan Campbell <me@kfcampbell.com>
2024-05-31 13:58:47 -0700

    AppInstallationTokenProvider basic ctor tests

--- test/Middleware/Authentication/AppInstallationTokenProviderTest.cs
+++ test/Middleware/Authentication/AppInstallationTokenProviderTest.cs
@@ -1,11 +1,24 @@
 using System.Security.Cryptography;
 using GitHub.Octokit.Client.Authentication;
-using Microsoft.IdentityModel.Tokens;
-using Moq;
 using Xunit;
 
 public class AppInstallationTokenProviderTests
 {
+    [Fact]
+    public void Constructor_InitializesTokenProviderWithClientId()
+    {
+        var privateKey = RSA.Create();
+        var tokenProvider = new AppInstallationTokenProvider("clientId", privateKey, "installationId");
 
-    // TODO: Add tests
+        Assert.NotNull(tokenProvider);
+    }
+
+    [Fact]
+    public void Constructor_InitializesTokenProviderWithAppId()
+    {
+        var privateKey = RSA.Create();
+        var tokenProvider = new AppInstallationTokenProvider(1, privateKey, "installationId");
+
+        Assert.NotNull(tokenProvider);
+    }
 }
